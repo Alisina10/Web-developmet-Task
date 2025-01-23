f92d26bdf206bd86a4bcbe87c6a1678700c01219
Alisina10 <jawidali2021@gmail.com>
2025-01-23 11:23:23 +0600

    new chnages

---
+++ Lab 5.py
@@ -0,0 +1,30 @@
+Name= "Ali Sina"
+print("My name is",Name)
+
+
+a=20
+b=5
+print("addition",a+b)
+print("subtraction", a-b)
+print("multiplication", a*b)
+print("division", a/b)
+
+x,y=10,5
+x,y=y,x
+print ("x=",x,"y=",y)
+
+
+pi=3.14
+radius=5
+area=pi*radius*radius
+print("area of circle",area)
+
+variable = input("Enter your name:")
+print("hellow", variable)
+AGE = int (input("Enter your age:"))
+print("your age is",AGE+1)
+
+
+a, b = input("Enter a number:"). split()
+a,b = int (a), int (b)
+print("the sum is", a+b)
