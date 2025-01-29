1f8e9162d737aac7c6b9066e464c76c8aca3a408
Alisina10 <jawidali2021@gmail.com>
2025-01-29 08:39:16 +0600

    done lAB 6

---
+++ lab 6.py
@@ -0,0 +1,37 @@
+# My lab Homework
+f_lab = float(input("enter a float number:"))
+i_lab = int (input("enter an integer number:"))
+s_lab =str (input("enter a string:"))
+
+print (type (f_lab))
+print (type (i_lab))
+print(type(s_lab))
+
+
+num = '1,2,3'
+integer = int (num)
+n_float = float (integer)
+
+print (integer)
+print (n_float)
+
+person ={
+    "name" : "ali",
+    "age" : 20,
+    "gender" : "male"
+
+}
+
+print (person["age"])
+Element = [1,2,3,4,5,6,7,8,9,10]
+Element.add = [11]
+Element.remove = [1]
+print (4 in Element)
+
+
+
+
+
+
+
+
