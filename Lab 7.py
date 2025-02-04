f100572de5d8e5462bf62a739be779679ba39e74
Alisina10 <jawidali2021@gmail.com>
2025-02-04 16:16:54 +0600

    done lAB 7

---
+++ Lab 7.py
@@ -0,0 +1,23 @@
+
+for i in range(11):
+        print(i)
+
+i = 1
+while i <= 10:
+    print(i)
+    i+=1
+
+
+text = "Python"
+for char in text:
+    print(char)
+
+i = 2
+while i < 10:
+    print(i)
+    i += 2
+
+
+
+
+
