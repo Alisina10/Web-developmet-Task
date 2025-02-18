4955296530ad7466191a1c1f5d6f4f3590c8091b
Alisina10 <jawidali2021@gmail.com>
2025-02-18 11:59:20 +0600

    done lAB 9

---
+++ Lab10.py
@@ -0,0 +1,51 @@
+import mymoudule
+mymoudule.greeting("Hello")
+
+person1 = {
+    "name": "John Doe",
+    "age": 30,
+    "city": "New York"
+
+}
+
+a = mymoudule.person1["name"]
+print(a)
+
+
+import mymoudule as mx
+a = mx.person1["name"]
+print(a)
+
+import platform
+x = platform.system()
+print(x)
+
+def greeting(name):
+    print("Hello, " + name)
+ person1 = {
+    "name": "John Doe",
+    "age": 30,
+    "city": "New York"
+ }
+
+ from Lab10 import person1
+ print(person1["name"])
+
+
+ import camelcase
+ text = "hello world"
+ print(camelcase.camelcase(text))
+ print(c.hump(text))
+
+
+ import math
+ y = math.ceil(1.5)
+ x = math.floor(1.5)
+ print(x,y)
+
+ x = math.pi
+ print(x)
+
+
+
+
