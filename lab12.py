93da6a8b8f35a161d47fcfbe138608ce70813b5f
Alisina10 <jawidali2021@gmail.com>
2025-03-01 22:37:55 +0600

    done lAB 12

--- lab12.py
+++ lab12.py
@@ -1,14 +1,38 @@
-company = {
-    "employee": [
-        {
-            "name": "Ravi", "position": "developer", "salary": 10000, "gender": "male", "location": "Egypt"
-        },
-        {
-            "name": "Ahmed", "position": "designer", "salary": 2000, "gender": "male", "location": "Egypt"
-        },
-    ]
-}
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
 
-    def deposit (self, amount ):
-            
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+    def  myfunction(abc):
+        print("hello my name is",abc.name)
+
+p1 = Person("ali",20)
+p1.myfunction()
+
+
+#polymoerphism Task
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def make_sound(self):
+        return "animal sound"
+
+class Dog(Animal):
+    def make_sound(self):
+        return "bark"
+
+class Cat(Animal):
+    def make_sound(self):
+        return "meow"
+
+
+dog = Dog("Fido")
+cat = Cat("Fluffy")
+
+print(dog.name, "says:",dog.make_sound())
+print(cat.name,"says :", cat.make_sound())
 
