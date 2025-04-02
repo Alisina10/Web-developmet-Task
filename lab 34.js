a89508a69136c9392ce8115b4da853e073193534
Alisina10 <jawidali2021@gmail.com>
2025-04-02 22:00:47 +0600

    Done all css lab

---
+++ lab 34.js
@@ -0,0 +1,78 @@
+// lab 1
+
+let a = 10;
+let b = 3;
+
+console.log("addition:", a+b);
+console.log("Subtraction:", a-b);
+console.log("Multipilication:", a*b);
+console.log("Division:", a/b);
+console.log("Modulus:", a % b);
+console.log("Expoentiation:", a+b);
+
+// lab 2
+
+let age = 15;
+let isRegistered = true;
+
+
+if (age >= 18 && isRegistered){
+  console.log ("you are Eligible to vote.")
+} else {
+  console.log ("you are not Eligible to vote");
+
+}
+
+// lab 3
+
+let a = 12;
+let b = 12;
+
+if (a > b){
+
+  console.log(" a is greater than b")
+} else if (a < b ){
+
+  console.log ( " b is greater than a"
+  )
+} else {
+  console.log ("a and b is Equal.")
+}
+
+// lab 4:
+
+let num = -4;
+
+if (num> 0 ){
+
+  console.log ("number is Positive.");
+} else if ( num < 0){
+  console.log ("number is Nagative.");
+} else {
+  console.log("the number is zero");
+}
+
+// lab 5
+
+let day = "Tuesday";
+
+switch (day) {
+  case "Monday":
+    console.log("Start of the week!");
+    break;
+  case "Friday":
+    console.log("Weekend is near!");
+    break;
+  case "Sunday":
+    console.log("It's a rest day.");
+    break;
+  default:
+    console.log("It's a regular day.");
+}
+
+// lab 6
+
+let num = 7;
+
+let result = (num % 2 === 0) ? "Even" : "Odd";
+console.log("The number is:", result);
---
+++ lab33.js
@@ -0,0 +1,35 @@
+// lab 1
+
+var user = "ali";
+let age = 22;
+const country = "AFG";
+
+console.log(user, age, country);
+
+user = "Mujtaba";
+age = "30";
+
+console.log (user, age, country)
+
+// lab 2 
+
+let name = "jawid";
+let age = "22";
+let isStudent = true;
+let score = null;
+let address;
+
+console.log ("name:", name, "-typy;", typeof name);
+console.log ("age:", age, "-typy;", typeof age);
+console.log ("Is student:", isStudent, "-typy;", typeof score);
+console.log ("score:", score, "-typy;", typeof score);
+console.log ("address:", address, "-typy;", typeof address);
+
+// lab 3
+
+let userName = prompt ("Enter your name");
+let userAge = prompt ("Enter your age");
+
+alert ("Hello", + userName + "! you are " + userAge + "Years old.");
+
+
